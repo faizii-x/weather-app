8015507c7a59d3ef1a12e137f8404ae3b3a49e22
faizanramzan670 <faizanramzan670@gmail.com>
2024-09-26 16:03:40 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import "./App.css";
 import Landing from "./pages/landing";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <>
       <Landing />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-         
-        </Routes>
-      </BrowserRouter>
+     
     </>
   );
 }
